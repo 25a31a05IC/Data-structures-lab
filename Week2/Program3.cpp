2afde320b34870aac587655debaa2388623adf4a
25a31a05IC <mskumar81068@gmail.com>
2026-03-14 10:58:23 +0530

    Program3.cpp
    Fixes syntax errors and memory allocation in Program3.cpp.

---
+++ Week2/Program3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a[10],n,i;
+cout<<"enter n value:";
+cin>>n;
+int *p=new int[n];
+for(i=o;i<n;i++)
+cin>>*(p+i);
+for(i=0;i<n;i++)
+cout<<*(p+i);
+delete[] p;
+}
